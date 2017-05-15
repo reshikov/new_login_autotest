2fff9c96b156e4a2324622b8cdc2033e5c858ad1
Igor Reshchikov <i.reshikov@gmail.com>
2017-05-15 23:29:15 +0300

    area

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,7 +3,28 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main (String[] args) {
-        System.out.println("Hello, Friend!");
+        hello("friend");
+        hello("user");
+        hello("Igor");
+
+        double l=5;
+        System.out.println("text " + l + " = " + area(l));
+
+        double a = 4;
+        double b = 6;
+        System.out.println("text 2 " + a + " and " + b + " = " + area (a,b));
+
     }
 
+    public static void hello (String somebody) {
+        System.out.println("Hello " + somebody + "!");
+    }
+
+    public static double area(double l) {
+        return l * l;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }
